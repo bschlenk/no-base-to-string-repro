efc0ec23488ae892c4a75fe1079878a8648d6883
Brian Schlenker <bschlenker@figma.com>
2026-01-08 11:27:36 -0500

    simplify repro

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import * as Helpers from './helpers.js'
 
-const something = { a: 'a', b: 'b' }
 
 export function main() {
-  console.log(Helpers.toString(something))
+  console.log(Helpers.toString({ a: 'a' }))
 }
 
